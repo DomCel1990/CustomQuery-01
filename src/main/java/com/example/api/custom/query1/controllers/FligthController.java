7b8183420f1723525315f0c46592c327abab4740
Domenico <menico.c90@gmail.com>
2022-10-25 21:31:26 +0200

    feat: CustomQuery1 modify

--- src/main/java/com/example/api/custom/query1/controllers/FligthController.java
+++ src/main/java/com/example/api/custom/query1/controllers/FligthController.java
@@ -4,9 +4,11 @@ import com.example.api.custom.query1.services.FligthService;
 import com.example.api.custom.query1.entities.Fligth;
 import com.example.api.custom.query1.repositoies.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fligths")
--- src/main/java/com/example/api/custom/query1/services/FligthService.java
+++ src/main/java/com/example/api/custom/query1/services/FligthService.java
@@ -4,20 +4,27 @@ import com.example.api.custom.query1.entities.Fligth;
 import com.example.api.custom.query1.entities.Status;
 import com.example.api.custom.query1.repositoies.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
 import java.util.Random;
+
 @Service
 public class FligthService {
 
     @Autowired
     FlightRepository flightRepository;
+
     public void createFlihts() {
         Random random = new Random();
         for (int i = 0; i < 50; i++) {
             String generatedString = random.ints(10, 97, 122)
                     //StringBuilder::new create an empty object String builder
-                    //StringBuilder:: new serve a creare un nuovo oggetto StringBuilder vuoto
                     //StringBuilder::appendCodePoint used to append the string representation of the codePoint argument to this sequence
                     .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                     .toString();
@@ -26,3 +33,4 @@ public class FligthService {
         }
     }
 }
+
